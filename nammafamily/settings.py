a187a61dc50c423fb000ee270b48bb195f22429b
Munirasu12345 <munirasulakshmi@gmail.com>
2026-03-10 14:53:02 +0530

    Fix indentation in INSTALLED_APPS

--- nammafamily/settings.py
+++ nammafamily/settings.py
@@ -28,28 +28,17 @@ CSRF_TRUSTED_ORIGINS = [
 # APPLICATIONS
 # --------------------------------------------------
 INSTALLED_APPS = [
+ Local apps first (so templates override admin)
+    'shop',
+
+    # Django contrib apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'shop',
-    'myapp',  # if you added this new app
-]
-#INSTALLED_APPS = [
-    # Local apps first (so templates override admin)
-#    'shop',
 
-    # Django contrib apps
-#    'django.contrib.admin',
-#    'django.contrib.auth',
-#    'django.contrib.contenttypes',
-#    'django.contrib.sessions',
-#    'django.contrib.messages',
-#    'django.contrib.staticfiles',
-#
     # Third-party
     'rest_framework',
 ]
